50a0e014f23dfe121dbe3a2ddfc5398e9a0c0cef
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-09-11 20:02:10 +0100

    Added documentation for knight

--- src/knight.cpp
+++ src/knight.cpp
@@ -1,8 +1,6 @@
 #include "../headers/pieces.h"
 #include "../headers/game.h"
 
-extern int abs(int);
-
 Knight::Knight(int x, int y, bool isBlack, SDL_Renderer* renderer, ChessBoard* board)
 	: Piece(x, y, isBlack, renderer, board)
 {
@@ -20,16 +18,25 @@ Knight::Knight(int x, int y, bool isBlack, SDL_Renderer* renderer, ChessBoard* b
 	}
 }
 
-bool Knight::canMove(int x_dest, int y_dest)
+/**
+ * canMove - Checks whether or not the knight piece moving to the grid
+ * specified by (x_dest, y_dest) is valid
+ *
+ * @x_dest: x-axis destination
+ * @y_dest: y_axis destonation
+ * @prot: Piece to ignore when checking for valid moves
+ *
+ * Return: true if a valid move was requested, false otherwise
+ */
+bool Knight::canMove(int x_dest, int y_dest, Piece* prot = nullptr)
 {
-	// Movement logic here
 	Piece* tmp;
 
 	tmp = m_board->getPiece(x_dest, y_dest);
 	if ((abs(y_dest - y) == 2 && abs(x_dest - x) == 1) ||
 			abs(x_dest - x) == 2 && abs(y_dest - y) == 1)
 	{
-		if (!tmp || (tmp && isOpponent(tmp)))
+		if (!tmp || (tmp && isOpponent(tmp)) || tmp == prot)
 			return (true);
 	}
 	return (false);
