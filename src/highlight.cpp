6a6893142e9da0d91c1ffff6d2c0412fdee25639
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-09-14 06:58:10 +0100

    Got rid of highlightSquare, highlightGrid, highlightCapture and replaced them with 'highlight'

--- src/highlight.cpp
+++ src/highlight.cpp
@@ -3,14 +3,15 @@
 #include "../headers/moves.h"
 
 /**
- * highlightSquare - Highlights a selected piece on the board
+ * highlight - Handles the highlighting of squares based on the highlight type
  *
  * @x: x position of piece to highlight
  * @y: y position of piece to highlight
+ * @type: Indicates the type of highlight to use
  *
  * Return- Nothing
  */
-void ChessBoard::highlightSquare(int x, int y)
+void ChessBoard::highlight(int x, int y, HighlightType type)
 {
 	SDL_Texture* h_texture;
 	SDL_Rect h_square;
@@ -20,10 +21,21 @@ void ChessBoard::highlightSquare(int x, int y)
 	p_offset = 5;
 	xpos = x * m_grid_size + m_board_pad + p_offset;
 	ypos = y * m_grid_size + m_board_pad + p_offset;
-	if ((x + y) % 2 == 0)
-		h_texture = loadImage("assets/active_black_piece.png", m_renderer);
-	else
-		h_texture = loadImage("assets/active_white_piece.png", m_renderer);
+
+
+	if (type == PIECE)
+		if ((x + y) % 2 == 0)
+			h_texture = loadImage("assets/active_black_piece.png", m_renderer);
+		else
+			h_texture = loadImage("assets/active_white_piece.png", m_renderer);
+	else if (type == MOVE)
+		h_texture = loadImage("assets/possible_squares.png", m_renderer);
+	else if (type == CAPTURE)
+		h_texture = loadImage("assets/possible_capture.png", m_renderer);
+	else if (type == CASTLING)
+		h_texture = loadImage("assets/castling_square.png", m_renderer);
+
+	// Throw exception if asset could not be loaded
 	if (h_texture == nullptr)
 	{
 		SDL_Log("Failed to load active_piece image!!!");
@@ -39,108 +51,6 @@ void ChessBoard::highlightSquare(int x, int y)
 	SDL_SetRenderTarget(m_renderer, nullptr);
 }
 
-/**
- * highlightGrid - Highlights possible squares for a piece to move to on the
- * board
- *
- * @x: x position of grid on the board
- * @y: y position of grid on the board
- *
- * Return: Nothing
- */
-void ChessBoard::highlightGrid(int x, int y)
-{
-	SDL_Texture* h_texture;
-	SDL_Rect h_square;
-	float xpos, ypos;
-	int p_offset;
-
-	p_offset = 5;
-	xpos = x * m_grid_size + m_board_pad + p_offset;
-	ypos = y * m_grid_size + m_board_pad + p_offset;
-	h_texture = loadImage("assets/possible_squares.png", m_renderer);
-	if (h_texture == nullptr)
-	{
-		SDL_Log("Failed to load active_piece image!!!");
-		// Throw Exception
-		return;
-	}
-
-	h_square = { (int) xpos, (int) ypos, (m_grid_size - p_offset),
-		(m_grid_size - p_offset)};
-	SDL_SetRenderTarget(m_renderer, m_chess_board);
-	SDL_RenderCopy(m_renderer, h_texture, nullptr, &h_square);
-	SDL_RenderPresent(m_renderer);
-	SDL_SetRenderTarget(m_renderer, nullptr);
-}
-
-/**
- * highlightCapture - Highlights possible captures for a piece on the board
- *
- * @x: x position of grid on the board
- * @y: y position of grid on the board
- *
- * Return: Nothing
- */
-void ChessBoard::highlightCapture(int x, int y)
-{
-	SDL_Texture* h_texture;
-	SDL_Rect h_square;
-	float xpos, ypos;
-	int p_offset;
-
-	p_offset = 5;
-	xpos = x * m_grid_size + m_board_pad + p_offset;
-	ypos = y * m_grid_size + m_board_pad + p_offset;
-	h_texture = loadImage("assets/possible_capture.png", m_renderer);
-	if (h_texture == nullptr)
-	{
-		SDL_Log("Failed to load active_piece image!!!");
-		// Throw Exception
-		return;
-	}
-
-	h_square = { (int) xpos, (int) ypos, (m_grid_size - p_offset),
-		(m_grid_size - p_offset)};
-	SDL_SetRenderTarget(m_renderer, m_chess_board);
-	SDL_RenderCopy(m_renderer, h_texture, nullptr, &h_square);
-	SDL_RenderPresent(m_renderer);
-	SDL_SetRenderTarget(m_renderer, nullptr);
-}
-
-/**
- * highlightCastling - Highlight castling route for the king piece on the board
- *
- * @x: x position of grid on the board
- * @y: y position of grid on the board
- *
- * Return: Nothing
- */
-void ChessBoard::highlightCastling(int x, int y)
-{
-	SDL_Texture* h_texture;
-	SDL_Rect h_square;
-	float xpos, ypos;
-	int p_offset;
-
-	p_offset = 5;
-	xpos = x * m_grid_size + m_board_pad + p_offset;
-	ypos = y * m_grid_size + m_board_pad + p_offset;
-	h_texture = loadImage("assets/castling_square.png", m_renderer);
-	if (h_texture == nullptr)
-	{
-		SDL_Log("Failed to load active_piece image!!!");
-		// Throw Exception
-		return;
-	}
-
-	h_square = { (int) xpos, (int) ypos, (m_grid_size - p_offset),
-		(m_grid_size - p_offset)};
-	SDL_SetRenderTarget(m_renderer, m_chess_board);
-	SDL_RenderCopy(m_renderer, h_texture, nullptr, &h_square);
-	SDL_RenderPresent(m_renderer);
-	SDL_SetRenderTarget(m_renderer, nullptr);
-}
 /**
  * highlightRoute - Highlights the route of different squares a piece can move
  * to on the board
