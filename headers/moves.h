404ad2e494b87b83c258aeac02bdfc6421080e6b
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-09-14 06:50:13 +0100

    Added declaration for grid class to be used for sending coordinates information between functions

--- headers/moves.h
+++ headers/moves.h
@@ -13,21 +13,42 @@
  * @piece_type: Piece type
  */
 class Move {
-	private:
-		int from_x, to_x, from_y, to_y;
-		PieceType piece_type;
+private:
+	int from_x, to_x, from_y, to_y;
+	PieceType piece_type;
 
-	public:
-		Move();
-		int fromX(void) const { return (from_x); };
-		int fromY(void) const { return (from_y); };
-		int toX(void) const { return (to_x); };
-		int toY(void) const { return (to_y); };
-		PieceType getPieceType(void) const { return (piece_type); };
-		void setFromX(int);
-		void setFromY(int);
-		void setToX(int);
-		void setToY(int);
-		void setPieceType(PieceType pt) { this->piece_type = pt; };
+public:
+	Move();
+	int fromX(void) const { return (from_x); };
+	int fromY(void) const { return (from_y); };
+	int toX(void) const { return (to_x); };
+	int toY(void) const { return (to_y); };
+	PieceType getPieceType(void) const { return (piece_type); };
+	void setFromX(int);
+	void setFromY(int);
+	void setToX(int);
+	void setToY(int);
+	void setPieceType(PieceType pt) { this->piece_type = pt; };
 };
+
+/**
+ * Grid - Defines the position of a grid on the chess board
+ *
+ * @x: x-axis position of the grid on the chess board
+ * @y: y-axis position of the grid on the chess board
+ */
+class Grid {
+private:
+	int x, y;
+
+public:
+	Grid (int x_pos, int y_pos) : x(x_pos), y(y_pos) {};
+	int getX(void) const { return (x); };
+	int getY(void) const { return (y); };
+	void setX(int x) { this->x = x; };
+	void setY(int y) { this->y = y; };
+};
+
+Grid* straightIntercept(Piece*, Piece*, Piece*);
+Grid* diagonalIntercept(Piece*, Piece*, Piece*);
 #endif
