2b51ea6e05b0d557536c64898c102d5ff2813815
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-09-19 06:08:01 +0100

    Added declarations for intercept(A grid that intercepts an attack on a piece's king) functions

--- headers/pieces.h
+++ headers/pieces.h
@@ -3,14 +3,10 @@
 
 #include "view.h"
 #include <iostream>
-
-enum PieceType
-{
-	KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN, NONE
-};
+#include <vector>
+#include "moves.h"
 
 class ChessBoard;
-class Grid;
 
 /*
  * class Piece - The base class for all chess pieces
@@ -34,23 +30,52 @@ class Piece {
 	public:
 		Piece(int, int, bool, SDL_Renderer*, ChessBoard*);
 		~Piece();
+
 		virtual bool canMove(int, int, Piece* p = nullptr) = 0;
-		int getX() const { return (x); };
-		int getY() const { return (y); };
+
+		int getX() const
+		{
+			return (x);
+		};
+
+		int getY() const
+		{
+			return (y);
+		};
+		
 		void setX(int);
 		void setY(int);
+
 		// Debugging purposes
 		void printBoard(void);
-		bool isBlack(void) const { return (is_black); };
-		bool isWhite(void) const { return (!is_black); };
+
+		bool isBlack(void) const
+		{
+			return (is_black);
+		};
+
+		bool isWhite(void) const
+		{
+			return (!is_black);
+		};
+
 		bool isOpponent(const Piece*) const;
 		bool isCovered(void);
 		bool canAttack(Piece*);
-		Grid* interceptGrid(Piece*, Piece*);
+
 		bool operator==(const Piece*) const;
-		PieceType getPieceType(void) const { return (piece_type); };
-		SDL_Texture* getTexture(void) const { return (piece_texture); };
+
+		PieceType getPieceType(void) const
+		{
+			return (piece_type);
+		};
+
+		SDL_Texture* getTexture(void) const
+		{
+			return (piece_texture);
+		};
 };
+
 
 class King : public Piece {
 	private:
@@ -59,52 +84,154 @@ class King : public Piece {
 		Piece* m_attacker;
 	public:
 		King(int, int, bool, SDL_Renderer*, ChessBoard*);
-		bool canMove(int, int, Piece*) override;
-		void setAttacker(Piece* piece) { m_attacker = piece; };
-		Piece* getAttacker(void) const { return (m_attacker); };
-		void moved(void) { m_has_moved = true; };
-		void setCheck(bool on_check) { m_on_check = on_check; };
-		bool onCheck(void) const { return (m_on_check); };
-		bool hasMoved(void) const { return (m_has_moved); };
+		bool canMove(int, int, Piece* p = nullptr) override;
+
+		void setAttacker(Piece* piece)
+		{
+			m_attacker = piece;
+		};
+
+		Piece* getAttacker(void) const
+		{
+			return (m_attacker);
+		};
+
+		void moved(void)
+		{
+			m_has_moved = true;
+		};
+
+		void setCheck(bool on_check)
+		{
+			m_on_check = on_check;
+		};
+
+		bool onCheck(void) const
+		{
+			return (m_on_check);
+		};
+
+		bool hasMoved(void) const
+		{
+			return (m_has_moved);
+		};
 };
 
 class Queen : public Piece {
 	private:
 		int angle(int, int);
+		std::vector<Grid> m_intercept;
+		std::vector<Grid> interceptGrids(Piece*, Piece*);
 	public:
 		Queen(int, int, bool, SDL_Renderer*, ChessBoard*);
-		bool canMove(int, int, Piece*) override;
+		bool canMove(int, int, Piece* p = nullptr) override;
+		
+		std::vector<Grid> getIntercepts(void) const
+		{
+			return (m_intercept);
+		};
+		
+		void updateIntercept(Piece* att, Piece* def)
+		{
+			m_intercept = interceptGrids(att, def);
+		};
+
+		bool isIntercept(int, int) const;
 };
 
 class Rook : public Piece {
 	private:
 		bool m_has_moved;
+		Grid m_intercept;
+		Grid interceptGrid(Piece*, Piece*);
 	public:
 		Rook(int, int, bool, SDL_Renderer*, ChessBoard*);
-		bool canMove(int, int, Piece*) override;
-		void moved(void) { m_has_moved = true; };
-		bool hasMoved(void) const { return (m_has_moved); };
+		bool canMove(int, int, Piece* p = nullptr) override;
+		
+		void moved(void)
+		{
+			m_has_moved = true;
+		};
+		bool hasMoved(void) const
+		{
+			return (m_has_moved);
+		};
+		
+		Grid getIntercept(void) const
+		{
+			return (m_intercept);
+		};
+		
+		void updateIntercept(Piece* att, Piece* def)
+		{
+			m_intercept = interceptGrid(att, def);
+		};
+		
+		bool isIntercept(int, int) const;
 };
 
 class Bishop : public Piece {
 	private:
 		int angle(int, int);
+		std::vector<Grid> m_intercept;
+		std::vector<Grid> interceptGrids(Piece*, Piece*);
 	public:
 		Bishop(int, int, bool, SDL_Renderer*, ChessBoard*);
-		bool canMove(int, int, Piece*) override;
+		bool canMove(int, int, Piece* p = nullptr) override;
+		
+		std::vector<Grid> getIntercepts(void) const
+		{
+			return (m_intercept);
+		};
+		
+		void updateIntercept(Piece* att, Piece* def)
+		{
+			m_intercept = interceptGrids(att, def);
+		};
+		
+		bool isIntercept(int, int) const;
 };
 
 class Knight : public Piece {
+	private:
+		std::vector<Grid> m_intercept;
+		std::vector<Grid> interceptGrids(Piece*, Piece*);
 	public:
 		Knight(int, int, bool, SDL_Renderer*, ChessBoard*);
-		bool canMove(int, int, Piece*) override;
+		bool canMove(int, int, Piece* p = nullptr) override;
+		std::vector<Grid> getIntercepts(void) const
+		{
+			return (m_intercept);
+		};
+
+		void updateIntercept(Piece* att, Piece* def)
+		{
+			m_intercept = interceptGrids(att, def);
+		};
+
+		bool isIntercept(int, int) const;
 };
 
 class Pawn : public Piece {
+	private:
+		Grid m_intercept;
+		Grid interceptGrid(Piece*, Piece*);
 	public:
 		Pawn(int, int, bool, SDL_Renderer*, ChessBoard*);
-		bool canMove(int, int, Piece*) override;
+		bool canMove(int, int, Piece* p = nullptr) override;
 		bool hasMoved(void) const;
+
+		Grid getIntercept(void) const
+		{
+			return (m_intercept);
+		};
+		
+		void updateIntercept(Piece* att, Piece* def)
+		{
+			m_intercept = interceptGrid(att, def);
+		};
+		
+		bool isIntercept(int, int) const;
 };
 
 // Overload the << operator to print Piece objects
